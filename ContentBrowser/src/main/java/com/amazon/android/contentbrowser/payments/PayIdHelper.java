d40e3ca72869d17d3972085212b025c05f01684c
Chris Buonocore <chrisdistrict@gmail.com>
2020-10-04 09:43:40 -0400

    add payid

--- ContentBrowser/src/main/java/com/amazon/android/contentbrowser/payments/PayIdHelper.java
+++ ContentBrowser/src/main/java/com/amazon/android/contentbrowser/payments/PayIdHelper.java
@@ -1,5 +1,17 @@
 package com.amazon.android.contentbrowser.payments;
 
+import java.math.BigInteger;
+import java.util.List;
+
+import io.xpring.common.XrplNetwork;
+import io.xpring.payid.PayIdClient;
+import io.xpring.payid.XrpPayIdClient;
+import io.xpring.payid.generated.model.Address;
+import io.xpring.payid.generated.model.CryptoAddressDetails;
+import io.xpring.xpring.XpringClient;
+import io.xpring.xrpl.Wallet;
+import io.xpring.xrpl.XrpClient;
+
 public class PayIdHelper {
 
 
@@ -11,10 +23,10 @@ public class PayIdHelper {
     static final String ERROR = "Error";
 
     // The XRP Ledger network to resolve on.
-//    static final XrplNetwork xrpNetwork = XrplNetwork.TEST;
+    static final XrplNetwork xrpNetwork = XrplNetwork.TEST;
 
     // The BTC network to resolve on.
-//    static final XrpClient xrpClient = new XrpClient(XRP_URL, xrpNetwork);
+    static final XrpClient xrpClient = new XrpClient(XRP_URL, xrpNetwork);
 
     //    String payId = "alice$dev.payid.xpring.money";
     public static String submitPayment(String payId, PaymentType paymentType, String destinationPayId, String amount) throws Exception {
@@ -40,45 +52,44 @@ public class PayIdHelper {
 
     private static String sendXrp(String payId, String walletSeed, String destinationPayId, String amount) throws Exception {
 
-//        final boolean isTest = XrplNetwork.TEST.equals(xrpNetwork);
-//        final Wallet wallet = new Wallet(walletSeed, isTest);
-//
-//        // The number of drops to send.
-//        BigInteger dropsToSend = BigInteger.valueOf(Long.parseLong(amount));
-//
-//
-//        // A client to resolve PayIDs on the XRP Ledger.
-//        XrpPayIdClient xrpPayIdClient = new XrpPayIdClient(xrpNetwork);
-//        System.out.println("Resolving PayID: " + payId);
-//        System.out.println("On network: " + xrpNetwork);
-//        String xrpAddress = xrpPayIdClient.xrpAddressForPayId(payId);
-//        System.out.println("Resolved to " + xrpAddress);
-//        System.out.println("");
-//
-//        XpringClient xpringClient = new XpringClient(xrpPayIdClient, xrpClient);
-//
-//        String hash = xpringClient.send(dropsToSend, destinationPayId, wallet);
-//        return hash;
-        return "";
+        final boolean isTest = XrplNetwork.TEST.equals(xrpNetwork);
+        final Wallet wallet = new Wallet(walletSeed, isTest);
+
+        // The number of drops to send.
+        BigInteger dropsToSend = BigInteger.valueOf(Long.parseLong(amount));
+
+
+        // A client to resolve PayIDs on the XRP Ledger.
+        XrpPayIdClient xrpPayIdClient = new XrpPayIdClient(xrpNetwork);
+        System.out.println("Resolving PayID: " + payId);
+        System.out.println("On network: " + xrpNetwork);
+        String xrpAddress = xrpPayIdClient.xrpAddressForPayId(payId);
+        System.out.println("Resolved to " + xrpAddress);
+        System.out.println("");
+
+        XpringClient xpringClient = new XpringClient(xrpPayIdClient, xrpClient);
+
+        String hash = xpringClient.send(dropsToSend, destinationPayId, wallet);
+        return hash;
     }
 
     private static String sendBtc(String payId, String amount) throws Exception {
         // A client to resolve PayIDs on any network..
-//        PayIdClient payIdClient = new PayIdClient();
-//
-//        System.out.println("Resolving PayID: " + payId);
-//        System.out.println("On network: " + btcNetwork);
-//        CryptoAddressDetails btcAddressComponents = payIdClient.cryptoAddressForPayId(payId, btcNetwork);
-//        System.out.println("Resolved to " + btcAddressComponents.getAddress());
-//        System.out.println("");
-//
-//
-//        System.out.println("Resolving All PayIDs for: " + payId);
-//        List<Address> allAddresses = payIdClient.allAddressesForPayId(payId);
-//        System.out.println("Resolved to:");
-//        for (int i = 0; i < allAddresses.size(); i++) {
-//            System.out.println(i + ") " + allAddresses.get(i));
-//        }
+        PayIdClient payIdClient = new PayIdClient();
+
+        System.out.println("Resolving PayID: " + payId);
+        System.out.println("On network: " + btcNetwork);
+        CryptoAddressDetails btcAddressComponents = payIdClient.cryptoAddressForPayId(payId, btcNetwork);
+        System.out.println("Resolved to " + btcAddressComponents.getAddress());
+        System.out.println("");
+
+
+        System.out.println("Resolving All PayIDs for: " + payId);
+        List<Address> allAddresses = payIdClient.allAddressesForPayId(payId);
+        System.out.println("Resolved to:");
+        for (int i = 0; i < allAddresses.size(); i++) {
+            System.out.println(i + ") " + allAddresses.get(i));
+        }
 
 
         throw new Exception("BTC payment not supported yet");
